20a993a4d3109d618e2085805166e8363bea6540
tyopoyt <dcrouseIII@gmail.com>
2023-06-12 13:33:12 -0400

    Load images on scroll, Add scroll to top button

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 import { PhotoTile } from 'src/app/models/photo-tile';
 import { ShuffleArrayPipe } from 'src/app/pipes/shuffle-array.pipe';
 import { PHONE_WIDTH } from 'src/app/services/window-size.service';
@@ -12,17 +12,21 @@ import { LightboxComponent } from '../lightbox/lightbox.component';
 export class GalleryComponent implements OnInit {
 
   @ViewChild('lightbox') lightbox: LightboxComponent;
+  @ViewChild('listContainer') listContainer: ElementRef;
   @Input() gallerySource: string = 'assets/film-gallery';
   @Input() numLandscape: number = 0;
   @Input() numPortrait: number = 0;
 
   photoTiles: PhotoTile[] = [];
   sortedTiles: PhotoTile[] = [];
+  shownTiles: PhotoTile[] = [];
+  tileChunk = 15;
   cols = 6;
   sorting: string;
   shuffleArrayPipe = new ShuffleArrayPipe();
   sortIcon: string = 'shuffle';
   sortText: string = 'Random';
+  atTop = true;
 
   @HostListener('window:resize', ['$event'])
   onResize() {
@@ -53,26 +57,47 @@ export class GalleryComponent implements OnInit {
       this.photoTiles.push(new PhotoTile(i, src, preview, rows, cols));
     }
 
-    this.nextSort();
+    this.nextSort(true);
+  }
+
+  onScroll = () => {
+    const element = this.listContainer.nativeElement
+    console.log(element)
+    if (Math.abs(element.scrollHeight - element.scrollTop - element.clientHeight) <= 500) {
+      this.shownTiles.push(...this.sortedTiles.slice(this.shownTiles.length, this.shownTiles.length + this.tileChunk))
+    }
+
+    this.atTop = element.scrollTop === 0;
   }
 
   showLightbox(index: number) {
     this.lightbox.show(index);
   }
 
-  nextSort() {
+  scrollToTop() {
+    this.listContainer.nativeElement.scrollTo({'top': 0, 'left': 0, 'behavior': 'smooth'});
+  }
+
+  nextSort(initialLoad: boolean = false) {
+    if(!initialLoad) {
+      this.scrollToTop();
+    }
+  
     if (this.sorting === 'shuffle') { // if shuffle, go to ascending
       this.sortedTiles = this.photoTiles.sort((a: PhotoTile, b: PhotoTile) => {return b.index - a.index} );
+      this.shownTiles = this.sortedTiles.slice(0, this.tileChunk);
       this.sorting = 'desc';
       this.sortIcon = 'calendar-asc';
       this.sortText = 'Newest';
     } else if (this.sorting === 'desc') { // if ascending, go to descending
       this.sortedTiles = this.photoTiles.sort((a: PhotoTile, b: PhotoTile) => {return a.index - b.index} );
+      this.shownTiles = this.sortedTiles.slice(0, this.tileChunk);
       this.sorting = 'asc';
       this.sortIcon = 'calendar-desc';
       this.sortText = 'Oldest'
     } else {// if descending, go to shuffle
       this.sortedTiles = this.shuffleArrayPipe.transform(this.photoTiles);
+      this.shownTiles = this.sortedTiles.slice(0, this.tileChunk);
       this.sorting = 'shuffle';
       this.sortIcon = 'shuffle';
       this.sortText = 'Random'
